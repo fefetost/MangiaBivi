1d9e11ed004a0f5452095447cc2a54dc292178c2
fefetost <tostofederico98@gmail.com>
2019-02-05 17:42:09 +0100

    Shared Preferences Handling

--- app/src/main/java/com/tosto/federico/mangiaebive/ui/activities/ShopActivity.java
+++ app/src/main/java/com/tosto/federico/mangiaebive/ui/activities/ShopActivity.java
@@ -1,5 +1,7 @@
 package com.tosto.federico.mangiaebive.ui.activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -17,19 +19,24 @@ import java.util.ArrayList;
 
 public class ShopActivity extends AppCompatActivity {
 
+    private static final String view_key = "view_KEY";
+    private static final String file_key = "com.tosto.federico.mangiaebive.key";
     RecyclerView recycler;
     RecyclerView.LayoutManager layoutManager;
     RestaurantAdapter adapter;
     ArrayList<Restaurant> arrayList;
-    boolean grid = false;
+    boolean grid=true;
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
         recycler = findViewById(R.id.recycleview);
-        layoutManager = new LinearLayoutManager(this);
+        sharedPreferences = this.getSharedPreferences(file_key, MODE_PRIVATE);
+        grid =sharedPreferences.getBoolean(view_key, false);
         adapter = new RestaurantAdapter(this, getData(), grid);
+        setLayoutManager();
         recycler.setAdapter(adapter);
         recycler.setLayoutManager(layoutManager);
     }
@@ -44,7 +51,7 @@ public class ShopActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.switch_view) {
-            if (setLayoutManager()) {
+            if (getLayoutManager()) {
                 item.setIcon(R.drawable.ic_view_list_black_24dp);
             } else {
                 item.setIcon(R.drawable.ic_view_module_black_24dp);
@@ -71,7 +78,7 @@ public class ShopActivity extends AppCompatActivity {
 
     }
 
-    private boolean setLayoutManager() {
+    private boolean getLayoutManager() {
         if (adapter.isGridMode()) {
             layoutManager = new LinearLayoutManager(this);
             grid = false;
@@ -83,6 +90,20 @@ public class ShopActivity extends AppCompatActivity {
         adapter.setGridMode(!adapter.isGridMode());
         recycler.setLayoutManager(layoutManager);
         recycler.setAdapter(adapter);
+        saveLayoutManager(grid);
         return grid;
     }
+
+    private void setLayoutManager(){
+        layoutManager= adapter.isGridMode()?new GridLayoutManager(this, 2):new LinearLayoutManager(this);
+    }
+
+    private void saveLayoutManager(boolean grid){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(view_key,grid);
+        if (editor.commit()){
+            Utility.showToast(this,"Salvato");
+        }
+    }
 }
+
